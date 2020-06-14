d8359c7313e00507bc95ba01affdceea60fbf2e6
2857944093 <2459899769@qq.com>
2020-06-14 23:28:43 +0800

    测试提交

--- springboot-blockchain/src/main/java/com/czk/hope/blockchain/eth/EthConstants.java
+++ springboot-blockchain/src/main/java/com/czk/hope/blockchain/eth/EthConstants.java
@@ -11,38 +11,38 @@ import java.math.BigInteger;
  * @Create: 2020/1/9 9:53
  * ETH配置类
  */
-public interface EthConstants {
+public class EthConstants {
 
     /*----- 自己维护一个nonce -----*/
-    BigInteger nonce = new BigInteger("0");
+    public static BigInteger nonce = new BigInteger("0");
 
-    String key = "";
+    public static String key = "";
 
     /*---- 建立web3j ----*/
-    Web3j web3j = Web3j.build(new HttpService("https://mainnet.infura.io/v3/" + key));
+    public static Web3j web3j = Web3j.build(new HttpService("https://mainnet.infura.io/v3/" + key));
 
     /*---- 转账的手续费 ----*/
-    BigInteger gasPrice = Convert.toWei("10", Convert.Unit.GWEI).toBigInteger();
+    public static BigInteger gasPrice = Convert.toWei("10", Convert.Unit.GWEI).toBigInteger();
     public static BigInteger gasLimit = Convert.toWei("45000", Convert.Unit.WEI).toBigInteger();
 
     /*---- 合约地址 ----*/
-    String contractAccount = "";
+    public static String contractAccount = "";
 
     /*---- 汇手续费的地址 ----*/
-    String serviceCharge = "";
+    public static String serviceCharge = "";
 
     /*---- 汇手续费的私钥 ----*/
-    String serviceChargePrivateKey = "";
+    public static String serviceChargePrivateKey = "";
 
     /*---- 代币的位数 ----*/
-    Integer decimals = 6;
+    public static Integer decimals = 6;
 
     /*---- 代币归集的地址 ----*/
-    String ercToAddress = "";
+    public static String ercToAddress = "";
 
     /*---- 获取私钥的URL ----*/
-    String getPrivateKeyUrl = "";
+    public static String getPrivateKeyUrl = "";
 
     /*---- 存放json私钥文件地址 ----*/
-    String privateKeyJsonFilePath = "D:\\wallet\\";
+    public static String privateKeyJsonFilePath = "D:\\wallet\\";
 }
